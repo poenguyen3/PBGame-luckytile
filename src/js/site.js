51af48f24b5bc5afe34c98fc2ae17f2aaed06059
Poe Nguyen <tantan.bamboo123@gmail.com>
2015-05-22 18:00:43 +0700

    Edit project on friday

---
+++ src/js/site.js
@@ -0,0 +1,13 @@
+(function($, window){
+  var socket = io();
+  socket.on('connect', function(){
+    socket.emit('hello there', 'HELLO NODE SERVER', function(data){
+      console.log(data);
+    });
+  });
+
+  $('.overlay').click(function(){
+    Waves.displayEffect();
+  });
+}(jQuery, window));
+$('body').attr("data-d","dlfkj");
